195e5148a92e6e360d2ec899b9c0496b2709b592
Simroc17 <simonerocco11@yahoo.it>
2022-10-20 11:27:14 +0200

    primo commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-
+<h1>Prova</h1>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
