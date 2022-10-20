e151e6bd1e4020d4bdc5ba0c478a3f0457ea0fbd
lorenzo10 <lor195@hotmail.it>
2022-10-20 11:44:49 +0200

    aggiunto: successo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 <h1>Prova</h1>
+<h3>successo</h3>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
